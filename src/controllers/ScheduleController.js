2212c07c3ec0c5f2f1c53523565d1f024555cd8b
xxziiko <xxziiko@gmail.com>
2024-11-24 17:36:44 +0900

    feat(ScheduleController): 휴일 비상 근무 순번대로 사원 닉네임을 입력 받는 기능

--- src/controllers/ScheduleController.js
+++ src/controllers/ScheduleController.js
@@ -1,16 +1,22 @@
-import { ErrorUtils } from '../utils/index.js';
-import { CalendarController } from './index.js';
+import { ErrorUtils, MESSAGE } from '../utils/index.js';
+import { CalendarController, StaffController } from './index.js';
 
 class ScheduleController {
   constructor() {
     this.calendarController = new CalendarController();
+    this.staffController = new StaffController();
   }
 
   async processSchedule() {
     const calendar = await ErrorUtils.getResult(() => this.calendarController.getCalendar());
     const staff = await ErrorUtils.getResult(() => this.getStaff());
+  }
+
   async getStaff() {
     const weekStep = await this.staffController.getStaff(MESSAGE.readWeekStep);
+    const holidayStep = await this.staffController.getStaff(MESSAGE.readHolidayStep);
+
+    return { weekStep, holidayStep };
   }
 }
 
